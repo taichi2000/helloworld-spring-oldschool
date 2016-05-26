88fae7697fb252cd8a75b2baf1020ce8589e3187
gasoto <gasoto@nisum.com>
2016-05-26 11:14:42 -0400

    added behavior to testGoodMorning

--- src/test/java/com/nisum/SpringAppTests.java
+++ src/test/java/com/nisum/SpringAppTests.java
@@ -1,15 +1,18 @@
 package com.nisum;
 
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 
 public class SpringAppTests {
@@ -17,14 +20,21 @@ public class SpringAppTests {
     private HelloService helloServiceOriginal;
     private HelloService helloServicePirata;
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() throws Exception {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
         helloServiceOriginal = (HelloService) context.getBean("helloServiceGERMAN");
-
         helloServicePirata = (HelloService) context.getBean("helloServiceGERMANAUTOBEAN");
+
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStream() {
+        System.setOut(null);
     }
 
 
@@ -43,5 +53,6 @@ public class SpringAppTests {
     @Test
     public void testGoodMorning() throws Exception {
         helloServiceOriginal.sayGoodMorning();
+        assertThat("Good Morning!!!!!", is(equalTo(outContent.toString().substring(0,outContent.size()-1))));
     }
 }
